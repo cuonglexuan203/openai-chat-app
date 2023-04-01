37fd01db26aa0b95ee4327645fe421e949cb2767
cuonglexuan203 <cuonglexuan203@gmail.com>
2023-04-02 06:08:40 +0700

    first upload

---
+++ client/src/components/chat/index.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+    useMultiChatLogic,
+    MultiChatSocket,
+    MultiChatWindow,
+} from "react-chat-engine-advanced";
+import Header from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForm/StandardMessageForm";
+import Ai from "@/components/customMessageForm/Ai";
+import AiCode from "@/components/customMessageForm/AiCode";
+import AiAssist from "@/components/customMessageForm/AiAssist";
+
+function Chat({ user, secret }) {
+    const chatProps = useMultiChatLogic(
+        import.meta.env.VITE_PROJECT_ID,
+        user,
+        secret
+    );
+    return (
+        <div style={{ flexBasis: "100%" }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow
+                {...chatProps}
+                style={{ height: "100vh" }}
+                renderChatHeader={(chat) => <Header chat={chat} />}
+                renderMessageForm={(props) => {
+                    if (chatProps.chat?.title.startsWith("AiChat_")) {
+                        return <Ai props={props} activeChat={chatProps.chat} />;
+                    }
+                    if (chatProps.chat?.title.startsWith("AiCode_")) {
+                        return (
+                            <AiCode props={props} activeChat={chatProps.chat} />
+                        );
+                    }
+                    if (chatProps.chat?.title.startsWith("AiAssist_")) {
+                        return (
+                            <AiAssist
+                                props={props}
+                                activeChat={chatProps.chat}
+                            />
+                        );
+                    }
+                    return (
+                        <StandardMessageForm
+                            props={props}
+                            activeChat={chatProps.chat}
+                        />
+                    );
+                }}
+            />
+        </div>
+    );
+}
+
+export default Chat;
